a343316a60f90c36b6c8deca86ffbfb4972a07fb
Maria Titova <Maria@MacBook-Pro-159.local>
2020-09-11 23:35:39 -0700

    TA section toggle added

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    $('a.TA1_classes').click(function() {
+        $(this).parent().parent().find(".TA1_classes.hidden").toggleClass('open');
+    });
+    $('a.TA2_classes').click(function() {
+        $(this).parent().parent().find(".TA2_classes.hidden").toggleClass('open');
     });
 });
